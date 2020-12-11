98666154f687c9a6c001591a5e8bcdf115b80092
huoyo <1729913829@qq.com>
2020-12-11 23:26:47 +0800

    修改

--- src/main/java/cn/langpy/kotime/util/Context.java
+++ src/main/java/cn/langpy/kotime/util/Context.java
@@ -6,6 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 public class Context {
 
     private static KoTimeConfig config;
+    static {
+        config = new KoTimeConfig();
+        config.setLogEnable(false);
+        config.setLogLanguage("chinese");
+    }
 
     public static void setConfig(KoTimeConfig koTimeConfig) {
         config = koTimeConfig;
